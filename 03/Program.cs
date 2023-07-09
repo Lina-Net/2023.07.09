4cd7d9c377fc7fcca74a890ef573baf3d2654e69
Ali <net_lina95@mail.ru>
2023-07-09 18:09:56 +0300

    дз готово

---
+++ 03/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: N =  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int m = 1; m <= N; m++)
+    Console.WriteLine(Math.Pow(m, 3));
